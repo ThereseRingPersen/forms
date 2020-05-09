1c4fd4f3d756cb1bd8c6c76f244ef132bc4c847b
Therese Persen <therese.persen@me.com>
2020-05-09 08:22:32 +0200

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header />
+        </div>
+    );
 }
 
 export default App;
