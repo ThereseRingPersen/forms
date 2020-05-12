4f93d02769e9d373a92d907fece35a975416a75c
Therese Persen <therese.persen@me.com>
2020-05-12 14:16:55 +0200

    global styling

---
+++ src/layout/colorPalette.ts
@@ -0,0 +1,17 @@
+const colorPalette = {
+    primary: '#5A0740',
+    secondary: '#B70E81',
+    alert: {
+        error: '#D32F2F',
+        success: '#00e600',
+    },
+    font: '#271622',
+    backGround: '#fffafa',
+    greyScale: {
+        grey1: '#F5F5F5',
+        grey2: '#E3E3E3',
+        grey3: '#C9C9C9',
+    },
+};
+
+export { colorPalette };
---
+++ src/layout/globalStyle.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+import { colorPalette } from './colorPalette';
+const GlobalStyle = createGlobalStyle`
+body {
+    *{
+        box-sizing: border-box;
+    }
+    display: flex;
+    flex-direction: column;
+    padding: 50px;
+    background: ${colorPalette.backGround};
+    font-family:Arial, Helvetica, sans-serif;
+    font-size:100%;
+    color: ${colorPalette.font};
+}
+
+h1 {
+    margin: 0.67em 0;
+    font-size: 2.5em;
+    font-weight: 100;
+   
+    }
+h2 {
+    margin: 0.83em 0;
+    font-size: 2em;
+    font-weight: 100; 
+    }
+h3{
+    margin: 0.83em 0;
+    font-size: 1.5em;
+    font-weight: 100;
+    }
+h4 {
+    margin: 0.5em 0;
+    font-size: 1.25em;
+    font-weight: 700;
+    }
+    
+p {
+    font-size: 1em;
+    }
+`;
+export default GlobalStyle;
