05165da5f6643025f2d5899bbf47782b7e70728a
Therese Persen <therese.persen@me.com>
2020-06-19 14:07:44 +0200

    clean up App, moved component to its own folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,15 @@ import styled from 'styled-components';
 import GlobalStyle from './styles/globalStyle';
 import TextField from './components/TextField/TextField';
 import Button from './components/Button/Button';
+import { CenteredLayout } from './styles/layout';
+import LoginForm from './components/Form/LoginForm/LoginForm';
 
 const App = () => {
-    const [field1, setfieldState1] = React.useState<boolean>(false); // controls label text size and labelposition.
-    const [value1] = React.useState<string>('');
     return (
-        <Layout>
+        <>
             <GlobalStyle />
-            <TextField
-                id={'input1'}
-                labelText={'Field1'}
-                hasError={true}
-                isActive={field1}
-                onChange={onChangeField1}
-                onBlur={onBlurField1}
-                onFocus={onFocusField1}
-                value={value1}
-            />
-            <Button>submit </Button>
-        </Layout>
+            <LoginForm />;
+        </>
     );
-    function onChangeField1(e: React.FormEvent<HTMLInputElement>) {
-        e.preventDefault();
-    }
-    function onBlurField1(e: React.FormEvent<HTMLInputElement>) {
-        e.preventDefault();
-        if (e.currentTarget.value.length > 0) {
-            setfieldState1(true);
-        } else {
-            setfieldState1(false);
-        }
-    }
-
-    function onFocusField1(e: React.FormEvent<HTMLInputElement>) {
-        e.preventDefault();
-        setfieldState1(true);
-    }
 };
-
 export default App;
-
-const Layout = styled.div`
-    display: flex;
-    flex-direction: column;
-`;
