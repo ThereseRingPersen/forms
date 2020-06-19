f998a6215ec979aa4e5fc5b7dfa46b0aac2119df
Therese Persen <therese.persen@me.com>
2020-06-19 11:34:34 +0200

    restructure Textfield component and test mvp helper text

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,39 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
 import Label from './Label';
 import TextInput from './TextInput';
-
-const FormGroup = styled.div`
-    position: relative;
-    margin-bottom: 2em;
-    height: 2.5em;
-`;
-
-interface TextFieldProps {
+import { formColors } from './formColors';
+import { InputGroup, FormGroup, HelperText } from './styles';
+type Type = 'text' | 'email' | 'password';
+interface TextFieldProps
+    extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     id: string;
+    type: Type;
     value?: string;
     labelText: string;
-    isActive: boolean;
+    active: boolean;
     hasError: boolean;
+    errorMessage?: string;
+    helperText?: string;
     onChange: (e: React.FormEvent<HTMLInputElement>) => void;
     onBlur?: (e: React.FormEvent<HTMLInputElement>) => void;
     onFocus?: (e: React.FormEvent<HTMLInputElement>) => void;
+    ref: any;
 }
 
 const TextField = (props: TextFieldProps) => {
     return (
         <FormGroup>
-            <TextInput
-                id={props.id}
-                isActive={props.isActive}
-                hasError={props.hasError}
-                onChange={props.onChange}
-                onBlur={props.onBlur}
-                onFocus={props.onFocus}
-            />
-            <Label htmlFor={props.id} isActive={props.isActive} hasError={props.hasError}>
-                {props.labelText}
-            </Label>
+            <InputGroup>
+                <TextInput
+                    id={props.id}
+                    isActive={props.active}
+                    hasError={props.hasError}
+                    onChange={props.onChange}
+                    onBlur={props.onBlur}
+                    onFocus={props.onFocus}
+                    type={props.type}
+                    ref={props.ref}
+                />
+                <Label htmlFor={props.id} isActive={props.active} hasError={props.hasError}>
+                    {props.labelText}
+                </Label>
+            </InputGroup>
+            <HelperText>Here is some helpertext</HelperText>
         </FormGroup>
     );
 };
