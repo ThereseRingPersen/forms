0e76119952ec322ec9b33a1c3308f1d801e66cef
Therese Persen <therese.persen@me.com>
2020-06-19 14:36:34 +0200

    move props declarations to separate file, as they do not belong in styles

---
+++ src/components/TextField/types.ts
@@ -0,0 +1,10 @@
+export interface LabelProps {
+    isActive: boolean;
+    children: string;
+    hasError: boolean;
+}
+
+export interface InputProps {
+    isActive: boolean;
+    hasError: boolean;
+}
