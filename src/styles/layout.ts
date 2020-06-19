c7076db91b3bfb36d5bf768dcf6cadc063ae3340
Therese Persen <therese.persen@me.com>
2020-06-19 11:32:44 +0200

    add a centered layout and move layouts to separate files

---
+++ src/styles/layout.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const CenteredLayout = styled.div`
+    width: 40vw;
+    padding: 2rem;
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    border: 0.1rem solid #4b0082;
+    border-radius: 0.313rem;
+    box-shadow: 3px 5px #eaccff;
+`;
