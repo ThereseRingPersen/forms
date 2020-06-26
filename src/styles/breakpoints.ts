79e728ce8e928d4dd075c5baa6b6dc637ef31c48
Therese Persen <therese.persen@me.com>
2020-06-26 14:16:16 +0200

    add css breakpoints for later use

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,11 @@
+const size = {
+    sm: '576px',
+    md: '768px',
+    lg: '992px',
+    xl: '1200px',
+};
+
+export const device = (Object.keys(size) as Array<keyof typeof size>).reduce((acc, key) => {
+    acc[key] = (style: String) => `@media (min-width: ${size[key]}) { ${style} }`;
+    return acc;
+}, {} as { [index: string]: Function });
