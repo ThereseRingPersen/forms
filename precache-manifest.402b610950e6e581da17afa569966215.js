ba03cbfeeaf59ed3ed20a26a6ae2bccff6ced142
Therese Persen <therese.persen@me.com>
2020-06-30 17:56:14 +0200

    Updates

---
+++ precache-manifest.402b610950e6e581da17afa569966215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4230bf73b3be22875fdcbe16b09718c",
+    "url": "/forms/index.html"
+  },
+  {
+    "revision": "2cec244fcbdd2fe3766b",
+    "url": "/forms/static/js/2.51bfad10.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/forms/static/js/2.51bfad10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e76b069caee65a4881a",
+    "url": "/forms/static/js/main.a8a8a2be.chunk.js"
+  },
+  {
+    "revision": "5f03308fa5d67841706f",
+    "url": "/forms/static/js/runtime-main.3973be70.js"
+  }
+]);
