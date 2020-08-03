e9915ca052cd22136cdffa83c2196f0a95368b36
Therese Persen <therese.persen@me.com>
2020-08-03 15:39:14 +0200

    use a single source of truth color palette

---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    primary: {
+        light: '#9c4dcc',
+        medium: '#6a1b9a',
+        dark: '#380063',
+    },
+    grey: {
+        light: '#eeeeee',
+        medium: '#e0e0e0',
+        dark: '#bdbdbd',
+        textDark: '#616161',
+    },
+    white: '#ffffff',
+    red: '#CC0000',
+    text: {
+        light: '#9e9e9e',
+        medium: '#616161',
+        dark: '#212121',
+    },
+};
