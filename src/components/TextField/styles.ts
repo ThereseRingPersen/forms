c1684af02190f492d24268669e734d196fbb6af0
Therese Persen <therese.persen@me.com>
2020-08-04 11:05:45 +0200

    change color field background, addnew line between functions

--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -14,33 +14,38 @@ export const Input = styled.input<InputProps>`
     outline: none;
     color: ${formColors.fieldText};
 `;
+
 export const InputGroup = styled.div<InputGroupProps>`
-    display: flex;
-    height: 3.6rem;
-    width: 18rem;
-    ${media.sm`
+           display: flex;
+           height: 3.6rem;
+           width: 18rem;
+           ${media.sm`
       width: 100%;
    `}
-    border-radius: 5px 5px 0 0;
-    box-shadow: ${(props) => (props.hasError ? `inset 0 -2.5px 0 0 ${formColors.border.error}` : `0`)};
-    background-color: ${(props) =>
-        props.isActive ? formColors.fieldBackround.active : formColors.fieldBackround.hover};
-    :hover {
-        box-shadow: ${(props) =>
-            props.hasError
-                ? `inset 0 -2.5px 0 0 ${formColors.border.error}`
-                : `inset 0 -1.5px 0 0 ${formColors.border.hover}`};
-    }
-    :focus-within,
-    :focus {
-        box-shadow: inset 0 -1.25px 0 0 ${(props) => (props.hasError ? formColors.border.error : formColors.border.focus)};
-    }
-`;
+           border-radius: 5px 5px 0 0;
+           background-color: ${formColors.fieldBackround.initial};
+           box-shadow: ${(props) => (props.hasError ? `inset 0 -2.5px 0 0 ${formColors.border.error}` : `0`)};
+           :hover {
+               background-color: ${formColors.fieldBackround.hover};
+               box-shadow: ${(props) =>
+                   props.hasError
+                       ? `inset 0 -2.5px 0 0 ${formColors.border.error}`
+                       : `inset 0 -1.5px 0 0 ${formColors.border.hover}`};
+           }
+
+           :focus-within,
+           :focus {
+               background-color: ${formColors.fieldBackround.active};
+               box-shadow: inset 0 -2.5px 0 0 ${(props) => (props.hasError ? formColors.border.error : formColors.border.focus)};
+           }
+       `;
+
 export const InputAndLabelContainer = styled.div<InputProps>`
     position: relative;
     width: 100%;
     flex-grow: 1;
 `;
+
 export const Label = styled.label<LabelProps>`
     position: absolute;
     height: 1.3rem;
@@ -51,16 +56,19 @@ export const Label = styled.label<LabelProps>`
     font-size: ${(props) => (props.isActive ? '0.8rem' : '1rem')};
     color: ${(props) => labelColor(props.hasError, props.isFocused)};
 `;
+
 export const HelperText = styled.div`
     color: ${formColors.helperText};
     font-size: 0.7rem;
     margin-top: 2.5px;
 `;
+
 export const ErrorText = styled.div`
     color: ${formColors.label.error};
     font-size: 0.7rem;
     margin-top: 2.5px;
 `;
+
 export const FormGroup = styled.div`
     margin-bottom: 14px;
     width: 18rem;
@@ -84,3 +92,4 @@ function labelColor(hasError: boolean, isFocused: boolean) {
     }
     return formColors.label.notActivated;
 }
+
